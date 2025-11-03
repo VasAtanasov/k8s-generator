e24ba0aa725b4ea1d7083ffe94c2171fc8d6bcc0
Vasil Atanasov <vas.atanasov@gmail.com>
2025-11-03 09:30:45 +0200

    Initial commit

---
+++ src/main/java/com/github/vaatech/k8s/generator/k8s_generator/Application.java
@@ -0,0 +1,13 @@
+package com.github.vaatech.k8s.generator.k8s_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
